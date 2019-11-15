06c88da5d7f44667e2e3012227d6e0d3abfb1d41
Jari-Pekka Ryynänen <jappi85@gmail.com>
2019-11-15 14:27:28 +0200

    Improve contract implementation

--- contracts/kps.sol
+++ contracts/kps.sol
@@ -2,11 +2,14 @@ pragma solidity ^0.5.0;
 
 contract KPS {
     enum Selection { NONE, ROCK, PAPER, SCISSORS }
-    enum Result { UNKNOWN, WINNER, LOSER, CHEATER, TIE }
 
     event GameStarted(address indexed addr, uint256 indexed gameIdentifier);
     event Revealed(uint256 indexed gameIdentifier, address addr, Selection selection);
-    event GameEnded(uint256 indexed gameIdentifier, Result opponentResult);
+
+    event Winner(uint256 indexed gameIdentifier, address addr);
+    event Cheater(uint256 indexed gameIdentifier, address addr);
+    event Tie(uint256 indexed gameIdentifier);
+
 
     struct Player {
         address payable addr;
@@ -17,9 +20,9 @@ contract KPS {
     }
 
     struct Game {
+        bool active;
         uint256 timestamp;
         Player[2] players;
-        address latestReveal;
     }
 
     uint256 private gameCounter;
@@ -36,6 +39,7 @@ contract KPS {
 
         Game storage game = games[currentGame];
 
+        game.active = true;
         game.players[playerIndex] = Player({
             addr: msg.sender,
             bet: msg.value,
@@ -50,10 +54,10 @@ contract KPS {
         emit GameStarted(msg.sender, currentGame);
     }
 
-    function reveal(uint256 gameIdentifier, uint256 nonce, Selection selection) public returns (Result) {
+    function reveal(uint256 gameIdentifier, uint256 nonce, Selection selection) public {
         Game storage game = games[gameIdentifier];
 
-        require(game.timestamp != 0, "Game is not active");
+        require(game.active, "Game is not active");
         require(selection != Selection.NONE, "Selection can not be NONE");
 
         (Player storage player, Player storage opponent) = getPlayers(game);
@@ -61,43 +65,39 @@ contract KPS {
         require(opponent.bet != 0, "Opponent has not placed a bet yet");
         require(player.selection == Selection.NONE, "Selection already revealed");
 
-        bool lastReveal = game.latestReveal != address(0);
         player.nonce = nonce;
-        game.latestReveal = msg.sender;
-        game.timestamp = now;
-
-        if (!isValidHash(nonce, msg.sender, selection, player.selectionHash)) {
-            payBets(game, [opponent, opponent]);
-            emit GameEnded(gameIdentifier, Result.CHEATER);
-            return Result.CHEATER;
-        }
-
         player.selection = selection;
+        game.timestamp = now;
 
         emit Revealed(gameIdentifier, msg.sender, selection);
 
-        Result result = Result.UNKNOWN;
-
-        if (lastReveal) {
-            result = resolveGame(player, opponent);
-            if (result == Result.WINNER) {
+        if (!isValidHash(nonce, msg.sender, selection, player.selectionHash)) {
+            payBets(game, [opponent, opponent]);
+            endGame(game);
+            emit Winner(gameIdentifier, opponent.addr);
+            emit Cheater(gameIdentifier, player.addr);
+        } else if (opponent.selection != Selection.NONE) {
+            address winner = getWinnerAddress(player, opponent);
+            if (winner == player.addr) {
                 payBets(game, [player, player]);
-            } else if (result == Result.LOSER) {
+                endGame(game);
+                emit Winner(gameIdentifier, player.addr);
+            } else if (winner == opponent.addr) {
                 payBets(game, [opponent, opponent]);
-            } else if (result == Result.TIE) {
+                endGame(game);
+                emit Winner(gameIdentifier, opponent.addr);
+            } else {
                 payBets(game, [player, opponent]);
+                endGame(game);
+                emit Tie(gameIdentifier);
             }
-            emit GameEnded(gameIdentifier, result);
         }
-
-        return result;
     }
 
     function collectAfterTimeout(uint256 gameIdentifier) public {
         Game storage game = games[gameIdentifier];
-        require(game.timestamp != 0, "Game is not active");
+        require(game.active, "Game is not active");
         require((game.timestamp + 3 minutes) < now, "Game is not yet in timeout");
-        require(game.latestReveal == msg.sender, "Only latest revealer can call this");
 
         (Player storage player, ) = getPlayers(game);
 
@@ -132,41 +132,44 @@ contract KPS {
             receivers[idx].addr.transfer(game.players[idx].bet);
             game.players[idx].bet = 0;
         }
-        game.timestamp = 0;
     }
 
-    function resolveGame(Player storage player, Player storage opponent) private view returns (Result) {
+    function endGame(Game storage game) private {
+        game.active = false;
+    }
+
+    function getWinnerAddress(Player storage player, Player storage opponent) private view returns (address) {
         if (player.selection == opponent.selection) {
-            return Result.TIE;
+            return address(0);
         }
 
         if (player.selection == Selection.ROCK) {
             if (opponent.selection == Selection.SCISSORS) {
-                return Result.WINNER;
+                return player.addr;
             }
             if (opponent.selection == Selection.PAPER) {
-                return Result.LOSER;
+                return opponent.addr;
             }
         }
 
         if (player.selection == Selection.PAPER) {
             if (opponent.selection == Selection.ROCK) {
-                return Result.WINNER;
+                return player.addr;
             }
             if (opponent.selection == Selection.SCISSORS) {
-                return Result.LOSER;
+                return opponent.addr;
             }
         }
 
         if (player.selection == Selection.SCISSORS) {
             if (opponent.selection == Selection.PAPER) {
-                return Result.WINNER;
+                return player.addr;
             }
             if (opponent.selection == Selection.ROCK) {
-                return Result.LOSER;
+                return opponent.addr;
             }
         }
 
-        return Result.UNKNOWN;
+        revert("Unable to resolve the game");
     }
 }
--- test/kps.js
+++ test/kps.js
@@ -1,6 +1,16 @@
 const KPS = artifacts.require('KPS');
 
 contract('KPS', accounts => {
+  const VALID_BET_VALUE = web3.utils.toWei('1', 'finney');
+
+  const GAS_PRICE = 20000000000; // This is the default for truffle test network
+
+  const SELECTION = {
+    ROCK: 1,
+    PAPER: 2,
+    SCISSORS: 3,
+  }
+
   function calculateHash(nonce, addr, selection) {
     return web3.utils.soliditySha3(
       { type: 'uint256', value: nonce },
@@ -9,28 +19,43 @@ contract('KPS', accounts => {
     );
   }
 
-  const validBetValue = web3.utils.toWei('1', 'finney');
-
-  const gasPrice = 20000000000; // This is the default for truffle develop network
+  async function createPlayers([selection0, selection1]) {
+    const players = [
+      { address: accounts[1], selection: selection0, nonce: 12345, expectedBalance: 0 },
+      { address: accounts[2], selection: selection1, nonce: 67890, expectedBalance: 0 },
+    ];
+    for (const player of players) {
+      player.expectedBalance = await web3.eth.getBalance(player.address);
+    }
+    return players;
+  }
 
   it('should play full fair game', async () => {
     const instance = await KPS.deployed();
-    const hash1 = calculateHash(12345, accounts[0], 1);
-    const hash2 = calculateHash(67890, accounts[1], 2);
-    let estimatedBalance1 = await web3.eth.getBalance(accounts[0]);
-    let estimatedBalance2 = await web3.eth.getBalance(accounts[1]);
+    const players = await createPlayers([SELECTION.ROCK, SELECTION.PAPER]);
+    const expectedWinner = players[1];
 
-    let transaction = await instance.startGame(hash1, { from: accounts[0], value: validBetValue });
-    estimatedBalance1 -= (transaction.receipt.cumulativeGasUsed * gasPrice);
-    transaction = await instance.startGame(hash2, { from: accounts[1], value: validBetValue });
-    estimatedBalance2 -= (transaction.receipt.cumulativeGasUsed * gasPrice);
+    for (const player of players) {
+      const hash = calculateHash(player.nonce, player.address, player.selection);
+      const { receipt } = await instance.startGame(hash, { from: player.address, value: VALID_BET_VALUE });
 
-    transaction = await instance.reveal(1, 12345, 1, { from: accounts[0] });
-    estimatedBalance1 -= (transaction.receipt.cumulativeGasUsed * gasPrice);
-    transaction = await instance.reveal(1, 67890, 2, { from: accounts[1] });
-    estimatedBalance2 -= (transaction.receipt.cumulativeGasUsed * gasPrice);
+      player.expectedBalance -= receipt.cumulativeGasUsed * GAS_PRICE;
+    }
 
-    console.log((await web3.eth.getBalance(accounts[0])) - estimatedBalance1)
-    console.log((await web3.eth.getBalance(accounts[1])) - estimatedBalance2)
+    const gameStartedEvents = await instance.getPastEvents('GameStarted');
+    const gameIdentifier = gameStartedEvents[0].returnValues.gameIdentifier;
+
+    for (const player of players) {
+      const { receipt } = await instance.reveal(gameIdentifier, player.nonce, player.selection, { from: player.address });
+
+      player.expectedBalance -= receipt.cumulativeGasUsed * GAS_PRICE;
+    }
+
+    for (const player of players) {
+      const accountBalance = await web3.eth.getBalance(player.address);
+      const expectedIncome = player === expectedWinner ? 1000000000000000 : -1000000000000000;
+
+      assert.equal(accountBalance - player.expectedBalance, expectedIncome);
+    }
   });
 }); 
