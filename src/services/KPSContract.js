27138b54ea9079e74f7d60f4367d34c43601eb28
Jari-Pekka Ryynänen <jappi85@gmail.com>
2019-11-19 17:25:21 +0200

    Use HttpProvider while developing

--- src/services/KPSContract.js
+++ src/services/KPSContract.js
@@ -1,28 +1,34 @@
 import Web3 from 'web3';
 import { abi } from 'contracts/KPS.json';
 
-const contractAddress = '0x6924D2D3939fC772DC85Df084c74132c4996Ac08';
+const contractAddress = '0xCfB3076E9008E812867B3C7AB300B8011AED6DF5';
 
 class KPSContract {
   initialise() {
-    this.web3 = new Web3(window.ethereum);
+    const provider = (
+      process.env.VUE_APP_BLOCKCHAIN_MODE === 'local' ?
+      new Web3.providers.HttpProvider('http://127.0.0.1:7545') :
+      window.ethereum
+    );
+    this.web3 = new Web3(provider);
   }
 
   async select(selection) {
     // eslint-disable-next-line
     console.log(selection);
     const nonce = this.nonce;
-    const accounts = await this.accounts();
+    const contract = await this.contract();
     const selectionHash = this.calculateSelectionHash(selection, nonce);
     // eslint-disable-next-line
     console.log(selectionHash);
     // eslint-disable-next-line
-    console.log(await this.contract.methods.startGame(selectionHash).send({from: accounts[0], to: contractAddress, value: this.web3.utils.toWei('1', 'finney')}));
+    console.log(await contract.methods.startGame(selectionHash).send({ value: this.web3.utils.toWei('1', 'finney') }));
   }
 
   async reveal(gameIdentifier, nonce, selection) {
-    const accounts = await this.accounts();
-    return await this.contract.methods.reveal(gameIdentifier, nonce, this.toSelectionEnum(selection)).send({from: accounts[0], to: contractAddress});
+    const contract = await this.contract();
+
+    return await contract.methods.reveal(gameIdentifier, nonce, this.toSelectionEnum(selection)).send();
   }
 
   async accounts() {
@@ -33,8 +39,10 @@ class KPSContract {
     return Math.floor(Math.random() * 1234567890);
   }
 
-  get contract() {
-      return new this.web3.eth.Contract(abi, contractAddress);
+  async contract() {
+    const accounts = await this.accounts();
+
+    return new this.web3.eth.Contract(abi, contractAddress, { from: accounts[0], gas: 1000000 });
   }
 
   toSelectionEnum(selection) {
