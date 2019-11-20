bf64fa055a079263c1889ddaef02f696792af355
Jari-Pekka Ryynänen <jappi85@gmail.com>
2019-11-20 17:05:46 +0200

    Use BigInt in contract test

--- test/kps.js
+++ test/kps.js
@@ -3,7 +3,7 @@ const KPS = artifacts.require('KPS');
 contract('KPS', accounts => {
   const VALID_BET_VALUE = web3.utils.toWei('1', 'finney');
 
-  const GAS_PRICE = 20000000000; // This is the default for truffle test network
+  const GAS_PRICE = BigInt(20000000000); // This is the default for truffle test network
 
   const SELECTION = {
     ROCK: 1,
@@ -20,11 +20,11 @@ contract('KPS', accounts => {
 
   async function createPlayers([selection0, selection1]) {
     const players = [
-      { address: accounts[1], selection: selection0, nonce: 12345, expectedBalance: 0 },
-      { address: accounts[2], selection: selection1, nonce: 67890, expectedBalance: 0 },
+      { address: accounts[3], selection: selection0, nonce: 12345, expectedBalance: 0 },
+      { address: accounts[4], selection: selection1, nonce: 67890, expectedBalance: 0 },
     ];
     for (const player of players) {
-      player.expectedBalance = await web3.eth.getBalance(player.address);
+      player.expectedBalance = BigInt(await web3.eth.getBalance(player.address));
     }
     return players;
   }
@@ -37,21 +37,20 @@ contract('KPS', accounts => {
     for (const player of players) {
       const hash = calculateHash(player.nonce, player.selection);
       const { receipt } = await instance.startGame(hash, { from: player.address, value: VALID_BET_VALUE });
-
-      player.expectedBalance -= receipt.cumulativeGasUsed * GAS_PRICE;
+      player.expectedBalance -= BigInt(receipt.gasUsed) * GAS_PRICE;
     }
 
-    const gameStartedEvents = await instance.getPastEvents('GameStarted');
-    const gameIdentifier = gameStartedEvents[0].returnValues.gameIdentifier;
+    const [ gameStartedEvent ] = await instance.getPastEvents('GameStarted');
+    const gameIdentifier = gameStartedEvent.returnValues.gameIdentifier;
 
     for (const player of players) {
       const { receipt } = await instance.reveal(gameIdentifier, player.nonce, player.selection, { from: player.address });
 
-      player.expectedBalance -= receipt.cumulativeGasUsed * GAS_PRICE;
+      player.expectedBalance -= BigInt(receipt.gasUsed) * GAS_PRICE;
     }
 
     for (const player of players) {
-      const accountBalance = await web3.eth.getBalance(player.address);
+      const accountBalance = BigInt(await web3.eth.getBalance(player.address));
       const expectedIncome = player === expectedWinner ? 1000000000000000 : -1000000000000000;
 
       assert.equal(accountBalance - player.expectedBalance, expectedIncome);
